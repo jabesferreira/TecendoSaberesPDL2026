28a72b34521c88d12087dd7df84823981b30eee2
jabesferreira <jabes.ferreira1538@gmail.com>
2026-04-05 23:22:12 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+      }
+    },
+  },
+  plugins: [],
+}
